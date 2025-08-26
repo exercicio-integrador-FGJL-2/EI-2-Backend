fe4f026eea829a611fea1f8f956bc97dfd264e20
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-08-26 13:32:08 -0300

    Arrumando os modelos, inserindo as relações entre os recursos.

--- src/Domain/Model/Funcionario.cs
+++ src/Domain/Model/Funcionario.cs
@@ -1,11 +1,15 @@
+using src.Domain.Model.Interface;
+
 namespace src.Domain.Model
 {
     public class Funcionario
     {
+        public required long Id { get; set; }
         public required long Matricula { get; set; }
         public required string Nome { get; set; }
         public string? Cargo { get; set; }
         //Data de admissao do funcionario
         public required DateTime DAdmissao { get; set; }
+        public List<IRecurso> RecursosAlocados { get; set; } = new List<IRecurso>();
     }
 }
--- src/Domain/Model/Interface/IRecurso.cs
+++ src/Domain/Model/Interface/IRecurso.cs
@@ -3,5 +3,6 @@ namespace src.Domain.Model.Interface
     public interface IRecurso
     {
         public long Id { get; set; }
+        public List<Funcionario> Funcionarios { get; set; } // Funcionarios que alocaram recursos.
     }
 }
