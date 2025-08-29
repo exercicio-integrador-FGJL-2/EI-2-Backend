faeb54677f8185564c633c63aa241509376c6167
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-08-29 10:37:13 -0300

    Get Alocacoes por recurso pronto

--- src/Persistence/Repositories/Interfaces/IRecursoFuncionarioRepository.cs
+++ src/Persistence/Repositories/Interfaces/IRecursoFuncionarioRepository.cs
@@ -6,7 +6,7 @@ namespace src.Persistence.Repositories.Interfaces
     public interface IRecursoFuncionarioRepository
     {
         Task<List<RecursoFuncionario>> GetAllGroupedByDateAsync(); //group by date
-        Task<int> GetAlocacoesPorRecurso(Recurso r);
+        Task<int> GetAlocacoesPorRecurso(Recurso tipo);
         Task<List<RecursoFuncionario>> GetByDateAsync(DateTime start, DateTime end);
         Task AlocarAsync(RecursoFuncionario recursoFuncionario);
 
